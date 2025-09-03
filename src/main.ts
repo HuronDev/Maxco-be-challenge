5c68bd9e597780edd68d2a8f4fbb8bcafe27accd
Huron <charlie.walls97@gmail.com>
2025-09-03 17:35:27 -0500

    refactor: Imporve the envisroment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Ventas')
+    .setDescription(
+      'API para gestión de clientes, zonas, productos, vendedores y ventas',
+    )
+    .setVersion('1.0')
+    .setContact('Carlos Andres Paredes', '', 'carlos.paredes23@hotmail.com')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useLogger(['error', 'warn', 'log']);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
